65eb3f9701b45b45388895abe4e600b8b8df2439
luohaosen <luohaosen@oriseq.com>
2026-08-31 09:11:27 +0800

    fix(.vuepress): 移除导航栏关于链接的图标和外部链接箭头样式
    - 移除了关于页面链接的图标配置
    - 删除了导航栏外部链接的箭头图标样式
    ```

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -7,7 +7,6 @@ export const zhNavbarConfig = navbar([
   "/navigation/",
   {
     text: "关于",
-    icon: "https://pic1.imgdb.cn/i/034EJkCaZlw3f8z0SxCckf.svg",
     link: "https://rochsen.github.io/Intro/",
   },
   //   "/about/",
